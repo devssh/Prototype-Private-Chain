3bc500f4a378ae17370a24dafe6d2d939f37608f
devssh <devasood@gmail.com>
2018-03-16 00:26:14 +0530

    Remove first block

--- src/main/java/app/controller/HomePageController.java
+++ src/main/java/app/controller/HomePageController.java
@@ -10,7 +10,7 @@ public class HomePageController {
         return "Greetings! Health Check success. \n" +
         "Checkout /coupons for read only initBlockchain data\n"+
         "Checkout /authorized for public keys of all authorities\n"+
-        "Checkout /create?message=Coupon208KX&owner=Dev&aadhar=122 to create block signed by Authority dev simulated on his server\n"+
+        "Checkout /create?sign=ASK_FOR_TOKEN&message=Coupon208KX&owner=Dev&aadhar=122 to create block signed by Authority dev simulated on his server\n"+
         "Checkout /verify-form to validate signature";
     }
 
